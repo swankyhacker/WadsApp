ab4152dd1cb6f7b401092f4d02fe6873b25a5f6f
Conalier <akshitkjain@gmail.com>
2020-06-18 18:44:38 +0700

    Finished the addTable method in Javascript

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let valBalance = document.getElementById("valBalance");
     const errBudget = document.getElementById("errBudget");
     const errExpense = document.getElementById("errExpense");
+    const expenseTable = document.getElementById("expenseTable");
 
     let budget = 0;
     let expense = 0;
@@ -54,7 +55,8 @@ document.addEventListener("DOMContentLoaded", () => {
             expenseValue.value = null;
             expenseInfo.value = null;
             updateBalance(log);
-            updateTable(recordExpense);
+            addTable(recordExpense);
+            counter++;
         }
     })
 
@@ -67,18 +69,49 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function updateBalance(log) {
         expense = 0;
+
         for (let i = 0; i < log.length; i++) {
             expense += parseInt(log[i].amount);
         }
-        console.log(expense);
+
         valExpenses.innerHTML = expense;
         balance = budget - expense;
         valBalance.innerHTML = balance;
     }
 
-    function updateTable(record) {
-        // create div elements to add to new blue flexbox
-        // use dataset attribute to uniquely identify each div
+    function addTable(record) {
+        const postRecord = document.createElement("div");
+        const editButton = document.createElement("a");
+        const deleteButton = document.createElement("a");
+        expenseTable.append(postRecord);
+        postRecord.innerHTML = `<span class = "title"> ${record.name} <span class = "value"> $${record.amount}`;
+        postRecord.className = "flex2-inner-item";
+        postRecord.append(editButton);
+        postRecord.append(deleteButton);
+        editButton.dataset.index = record.counter;
+        deleteButton.dataset.index = record.counter;
+        editButton.href = "#";
+        deleteButton.href = "#";
+        editButton.innerHTML = "Edit";
+        deleteButton.innerHTML = "Delete";
+        editButton.onclick = () => {
+            editTable();
+        };
+        deleteButton.onclick = () => {
+            deleteTable();
+        };
+    }
+
+    function editTable() {
+        console.log("YEET");
+    }
+
+    function deleteTable() {
+        console.log("Yeet");
     }
 
 })
+
+// create div elements to add to new blue flexbox
+// use dataset attribute to uniquely identify each div
+// correct table CSS
